912e0a4687e776ec93a13aa72659e6a49c8aea98
mhasaero <eroouji7314@gmail.com>
2024-08-22 10:56:49 +0700

    feat(root): navbar responsive

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,9 +7,7 @@ type Props = {
 export default function MainLayout({ children }: Props) {
   return (
     <div className="overflow-x-hidden bg-background">
-      <main className="mx-4 md:mx-12 md:py-6 lg:mx-16 xl:mx-24">
-        {children}
-      </main>
+      <main className="mx-4 md:mx-12 lg:mx-16 xl:mx-24">{children}</main>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import Image from "next/image";
-import { Search, Heart, ShoppingCart } from "lucide-react";
+import { Search, Heart, ShoppingCart, Menu } from "lucide-react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { routes } from "@/lib/link";
 import { cn } from "@/lib/utils";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function Navbar() {
   const pathname = usePathname();
@@ -15,15 +23,15 @@ export default function Navbar() {
       id="navbar"
       className="fixed left-0 right-0 flex items-center justify-between border-b-2 border-primary bg-background px-4 py-4 md:px-12 md:py-6 lg:px-16 xl:px-24"
     >
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-1 md:gap-3">
         <Image
           src={"/images/logo.png"}
           alt="logo"
           width={100}
           height={100}
-          className="size-8 xl:size-10"
+          className="size-6 md:size-8 xl:size-10"
         />
-        <h1 className="font-montserrat text-2xl text-primary xl:text-4xl">
+        <h1 className="font-montserrat text-lg text-primary md:text-2xl xl:text-4xl">
           <strong>Barkas Layo</strong>
         </h1>
       </div>
@@ -39,17 +47,52 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
-      <ul className="hidden gap-9 text-sm lg:flex xl:text-base">
-        <li>
-          <Search />
-        </li>
-        <li>
-          <Heart />
-        </li>
-        <li>
-          <ShoppingCart />
-        </li>
-      </ul>
+      <div className="flex justify-between gap-9">
+        <ul className="hidden gap-9 md:flex md:text-sm xl:text-base">
+          <li>
+            <Search className="size-6 md:size-8" />
+          </li>
+          <li>
+            <Heart className="size-6 md:size-8" />
+          </li>
+          <li>
+            <ShoppingCart className="size-6 md:size-8" />
+          </li>
+        </ul>
+        <Sheet>
+          <SheetTrigger className="lg:hidden">
+            <Menu className="size-6 text-primary md:size-8" />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="my-6 flex items-center justify-center gap-2">
+                <Image
+                  src={"/images/logo.png"}
+                  alt="logo"
+                  width={100}
+                  height={100}
+                  className="size-8"
+                />
+                <h1 className="font-montserrat text-xl text-primary">
+                  <strong>Barkas Layo</strong>
+                </h1>
+              </SheetTitle>
+              <ul className="flex flex-col items-start gap-6 text-base">
+                {routes.map((routes) => (
+                  <li
+                    key={routes.linkTo}
+                    className={cn("", {
+                      "text-primary underline": pathname === routes.address,
+                    })}
+                  >
+                    <Link href={routes.address}>{routes.linkTo}</Link>
+                  </li>
+                ))}
+              </ul>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
     </nav>
   );
 }
