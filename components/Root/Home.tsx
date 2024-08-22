36ae492950d6378f9bd3d7deb9c73e025f068dde
widya <widyadhana92@gmail.com>
2024-08-22 12:48:22 +0700

    fix: change homepage position

--- components/Root/Home.tsx
+++ components/Root/Home.tsx
@@ -4,9 +4,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <section id="home" className="h-screen">
-      <div className="absolute left-0 right-0 top-10 flex h-screen items-center bg-[url('/images/banner.png')] bg-cover bg-fixed md:top-20">
-        <div className="mx- flex justify-center md:mr-20 md:justify-end">
-          <div className="flex h-fit w-3/4 flex-col justify-between gap-4 rounded-2xl bg-background p-6 md:w-1/2 md:p-8">
+      <div className="absolute left-0 right-0 flex h-screen items-center bg-[url('/images/banner.png')] bg-cover bg-fixed top-0">
+        <div className="flex justify-center md:mr-20 md:justify-end">
+          <div className="flex h-fit w-3/4 flex-col justify-between gap-4 rounded-2xl bg-background p-6 md:w-1/2 md:p-8 mt-14">
             <div className="flex gap-2 text-xs font-semibold tracking-[3px] xl:text-sm">
               <h3>BERKAS</h3>
               <h3 className="text-yellow">LAYO</h3>
