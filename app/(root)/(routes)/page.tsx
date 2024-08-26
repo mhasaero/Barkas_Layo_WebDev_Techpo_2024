49038cdd9554e53b81ff58db161dfe40d0f2ace4
widya <widyadhana92@gmail.com>
2024-08-26 21:03:38 +0700

    fix: change 'home' to 'hero'

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,17 +1,17 @@
 import Navbar from "@/components/Navbar";
-import Home from "@/components/Root/Home";
 import Category from "@/components/Root/Category";
 import Recommendation from "@/components/Root/Recommendation";
 import Review from "@/components/Root/Review";
 import Gallery from "@/components/Root/Gallery";
 import Footer from "@/components/Footer";
+import Hero from "@/components/Root/Hero";
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <main className="flex flex-col gap-40">
-        <Home />
+        <Hero />
         <Review />
         <Gallery />
         <Footer />
--- components/Root/Hero.tsx
+++ components/Root/Hero.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function Hero() {
   return (
-    <section id="home" className="h-screen">
+    <section id="hero" className="h-screen">
       <div className="absolute left-0 right-0 flex h-screen items-center bg-[url('/images/banner.png')] bg-cover bg-fixed top-0">
         <div className="flex justify-center md:mr-20 md:justify-end">
           <div className="flex h-fit w-3/4 flex-col justify-between gap-4 rounded-2xl bg-background p-6 md:w-1/2 md:p-8 mt-14">
             <div className="flex gap-2 text-xs font-semibold tracking-[3px] xl:text-sm">
-              <h3>BERKAS</h3>
+              <h3>BARKAS</h3>
               <h3 className="text-yellow">LAYO</h3>
             </div>
             <h1 className="text-2xl font-bold text-primary md:text-4xl xl:text-5xl">
