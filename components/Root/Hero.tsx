27dbab5be6a6a5e65f864fab5e14f7c52717e1d4
widya <widyadhana92@gmail.com>
2024-08-27 22:26:45 +0700

    fix(root): fix hero section responsiveness on ipad/tab view

--- components/Root/Hero.tsx
+++ components/Root/Hero.tsx
@@ -5,8 +5,8 @@ export default function Hero() {
   return (
     <section id="hero" className="h-screen">
       <div className="absolute left-0 right-0 flex h-screen items-center bg-[url('/images/banner.png')] bg-cover bg-fixed top-0">
-        <div className="flex justify-center md:mr-20 md:justify-end">
-          <div className="flex h-fit w-3/4 flex-col justify-between gap-4 rounded-2xl bg-background p-6 md:w-1/2 md:p-8 mt-14">
+        <div className="flex justify-center lg:mr-20 lg:justify-end">
+          <div className="flex h-fit w-3/4 flex-col justify-between gap-4 rounded-2xl bg-background p-6 lg:w-1/2 lg:p-8 mt-14">
             <div className="flex gap-2 text-xs font-semibold tracking-[3px] xl:text-sm">
               <h3>BARKAS</h3>
               <h3 className="text-yellow">LAYO</h3>
@@ -14,7 +14,7 @@ export default function Hero() {
             <h1 className="text-2xl font-bold text-primary md:text-4xl xl:text-5xl">
               Temukan Barang Bekas Berkualitas di Sekitarmu!
             </h1>
-            <p className="mb-2 text-sm font-semibold xl:text-base">
+            <p className="mb-2 text-sm md:text-base font-semibold">
               Dari gadget hingga furniture, semua ada di sini. Pilih barang
               bekas impianmu dengan mudah dan cepat.
             </p>
