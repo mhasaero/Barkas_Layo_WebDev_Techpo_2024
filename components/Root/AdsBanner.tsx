7985a1c969392606421039b13d2dd938fb134b97
widya <widyadhana92@gmail.com>
2024-08-28 20:16:56 +0700

    feat(root): add more ads on second-third slide banner

--- components/Root/AdsBanner.tsx
+++ components/Root/AdsBanner.tsx
@@ -42,19 +42,19 @@ export default function AdsBanner() {
             <CarouselContent>
               <CarouselItem>
                 <div>
-                  <div className="h-[50vh] lg:h-44 xl:h-52 w-full rounded-xl bg-lighterBlue">
+                <div className="h-80 lg:h-44 xl:h-52 w-full rounded-xl bg-lighterBlue">
                     <div className="flex flex-col lg:flex-row h-full w-full">
-                      <div className="h-full w-full lg:w-1/2">
-                        <div className="h-full w-full rounded-t-xl lg:rounded-l-xl bg-[url('/images/kimbab-banner.png')] bg-cover bg-center"></div>
+                      <div className="min-h-36 lg:h-full w-full lg:w-1/2">
+                        <div className="h-full w-full rounded-t-xl lg:rounded-r-none lg:rounded-l-xl bg-[url('/images/kimbab-banner.png')] bg-cover bg-center"></div>
                       </div>
-                      <div className="h-44 lg:h-full w-full lg:w-1/2 py-5 lg:py-0 px-5 flex flex-col  lg:justify-center gap-2">
+                      <div className="lg:h-full w-full lg:w-1/2 py-5 lg:py-0 px-5 flex flex-col  lg:justify-center gap-2">
                         <div className="flex gap-2">
                           <h1 className="text-sm md:text-base lg:text-xl xl:text-2xl text-muted-foreground">
                             PROMO KEMERDEKAAN
                           </h1>
                           <h1 className="text-sm md:text-base lg:text-xl xl:text-2xl text-primary">KIMBAB YEAY</h1>
                         </div>
-                        <div className="flex flex-col gap-1">
+                        <div className="flex flex-col  gap-1">
                           <h1 className="text-xl lg:text-2xl xl:text-3xl font-bold text-primary">
                             BELI 7 KIMBAB + FREE 2 KIMBAB
                           </h1>
@@ -75,27 +75,33 @@ export default function AdsBanner() {
               </CarouselItem>
               <CarouselItem>
                 <div>
-                  <div className="h-[50vh] lg:h-44 xl:h-52 w-full rounded-xl bg-lighterBlue">
+                  <div className="h-80 lg:h-44 xl:h-52 w-full rounded-xl bg-lighterBlue">
                     <div className="flex flex-col lg:flex-row h-full w-full">
-                      <div className="h-full w-full lg:w-1/2">
-                        <div className="h-full w-full rounded-t-xl lg:rounded-l-xl bg-[url('/images/kimbab-banner.png')] bg-cover bg-center"></div>
+                      <div className="min-h-36 lg:h-full w-full lg:w-1/2">
+                        <div className="h-full w-full rounded-t-xl lg:rounded-r-none lg:rounded-l-xl bg-[url('/images/kost-banner.png')] bg-cover bg-center"></div>
                       </div>
-                      <div className="h-44 lg:h-full w-full lg:w-1/2 py-5 lg:py-0 px-5 flex flex-col  lg:justify-center gap-2">
+                      <div className="lg:h-full w-full lg:w-1/2 py-3 px-4 md:py-5 md:px-5 lg:py-0  flex flex-col  lg:justify-center gap-1 lg:gap-2">
                         <div className="flex gap-2">
                           <h1 className="text-sm md:text-base lg:text-xl xl:text-2xl text-muted-foreground">
-                            PROMO KEMERDEKAAN
+                          TAKE OVER KAMAR
                           </h1>
-                          <h1 className="text-sm md:text-base lg:text-xl xl:text-2xl text-primary">KIMBAB YEAY</h1>
+                          <h1 className="text-sm md:text-base lg:text-xl xl:text-2xl text-primary"></h1>
                         </div>
                         <div className="flex flex-col gap-1">
                           <h1 className="text-xl lg:text-2xl xl:text-3xl font-bold text-primary">
-                            BELI 7 KIMBAB + FREE 2 KIMBAB
+                          KOST MUSLIMAH 2
                           </h1>
                           <p className="text-xs text-muted-foreground">
-                            *Hanya berlaku pada 17 Agustus 2024
+                          *Khusus Putri
                           </p>
                           <p className="text-xs text-muted-foreground">
-                            *Lokasi di depan kampus UNSRI Layo
+                          *Dekat UNSRI (Gg. Buntu)
+                          </p>
+                          <p className="text-xs text-muted-foreground">
+                          *Periode Agustus-Desember (Alasan mau magang)
+                          </p>
+                          <p className="text-xs font-bold text-primary">
+                          Selengkapnya Hubungi: 0853-7812-1234
                           </p>
                         </div>
                       </div>
@@ -108,28 +114,23 @@ export default function AdsBanner() {
               </CarouselItem>
               <CarouselItem>
                 <div>
-                  <div className="h-[50vh] lg:h-44 xl:h-52 w-full rounded-xl bg-lighterBlue">
+                  <div className="h-80 lg:h-44 xl:h-52 w-full rounded-xl bg-[#D9D4D0]">
                     <div className="flex flex-col lg:flex-row h-full w-full">
-                      <div className="h-full w-full lg:w-1/2">
-                        <div className="h-full w-full rounded-t-xl lg:rounded-l-xl bg-[url('/images/kimbab-banner.png')] bg-cover bg-center"></div>
+                      <div className="min-h-36 lg:h-full w-full lg:w-1/2">
+                        <div className="h-full w-full rounded-t-xl lg:rounded-r-none lg:rounded-l-xl bg-[url('/images/ads-banner.png')] bg-cover bg-center"></div>
                       </div>
-                      <div className="h-44 lg:h-full w-full lg:w-1/2 py-5 lg:py-0 px-5 flex flex-col  lg:justify-center gap-2">
-                        <div className="flex gap-2">
-                          <h1 className="text-sm md:text-base lg:text-xl xl:text-2xl text-muted-foreground">
-                            PROMO KEMERDEKAAN
-                          </h1>
-                          <h1 className="text-sm md:text-base lg:text-xl xl:text-2xl text-primary">KIMBAB YEAY</h1>
-                        </div>
-                        <div className="flex flex-col gap-1">
-                          <h1 className="text-xl lg:text-2xl xl:text-3xl font-bold text-primary">
-                            BELI 7 KIMBAB + FREE 2 KIMBAB
-                          </h1>
-                          <p className="text-xs text-muted-foreground">
-                            *Hanya berlaku pada 17 Agustus 2024
-                          </p>
-                          <p className="text-xs text-muted-foreground">
-                            *Lokasi di depan kampus UNSRI Layo
-                          </p>
+                      <div className="flex  lg:relative lg:h-full w-full lg:w-1/2 py-2 px-1 md:py-5 md:px-5 lg:py-0 xl:-ml-6 items-center justify-center">
+                        <div className="lg:absolute xl:flex lg:w-[475px] xl:w-auto bottom-auto lg:-left-16 xl:-left-0 flex flex-col gap-3 md:items-center md:gap-8 lg:gap-3 lg:border-[1px] border-primary py-1 px-2 lg:py-2 lg:px-4 xl:py-3 xl:px-6 rounded-lg">
+                          <div className="flex flex-col md:flex-row md:gap-2 text-center text-sm md:text-base lg:text-xs xl:text-sm">
+                            <h5 className="text-muted-foreground font-medium ">Ingin memasarkan produk?  </h5>
+                            <h5 className="text-primary font-bold">Hubungi kami</h5>
+                            <h5 className="text-muted-foreground font-medium">untuk jasa iklan!</h5>
+                          </div>
+                          <div className="flex flex-col md:flex-row gap-1 lg:gap-2 text-xl lg:text-xl xl:text-2xl font-semibold text-primary text-center">
+                          <h1>0853-1234-5678 </h1>
+                          <h1>(Admin Barkas)</h1>
+                          </div>
+
                         </div>
                       </div>
                     </div>
